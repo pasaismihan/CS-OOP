454111e482f7f366028049f9efb26ff630fb63a9
pasaismihan <pasa_13142@hotmail.com>
2023-08-01 23:49:55 +0300

    Degiskenler

---
+++ CS_BASICS/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace CsTemelleri
+{
+    class CsTemelleriIlkDers
+    {
+        public void Main (string[] arg) 
+        {
+            #region DegiskenTanimlama
+            string Ad; //degisken tanimladik
+                       // "pasa" cift tirnak ile tanimliysa stringdir
+                       // 'p' tek tirnak ile tanimliysa char dir 
+            #endregion
+            #region DegiskenDegerAtama
+            string Ad1 = "pasa"; //degiskeni tanimladik ve deger atadik ayni satirda
+            string Ad2;
+            Ad2 = "ismihan"; // degiskeni tanimladik fakat deger atamasini ayrica yaptik  
+            #endregion
+
+
+        }
+    }
+}
